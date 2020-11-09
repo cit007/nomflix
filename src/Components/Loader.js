559c93c916ccb03f2a4bbe57e16edd8c415a8718
cit007 <itcho0077@gmail.com>
2020-11-10 02:20:47 +0900

    add react-loadingg library

--- src/Components/Loader.js
+++ src/Components/Loader.js
@@ -1,10 +1,4 @@
 import React from "react";
-import styled from "styled-components";
+import { CircleToBlockLoading } from 'react-loadingg';
 
-const Container = styled.div`
-  height: 100vh;
-  width: 100vw;
-  margin: 10px;
-`;
-
-export default () => <Container>Loading...</Container>;
+export default () => <CircleToBlockLoading />;
